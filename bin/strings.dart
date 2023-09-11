95da967c24391f5cb83c98e086cacfb72d2e660a
KhadafiZubaidiDev <khadafizubaidi.dev@gmail.com>
2023-09-11 20:35:02 +0700

    Numbers and Strings

---
+++ bin/strings.dart
@@ -0,0 +1,10 @@
+void main() {
+  print('"What do you think of Dart?" he asked');
+  print('"I think it\'s great!" I answered confidently');
+  print("Windows path: C:\\Program Files\\Dart");
+  var name = 'Messi';
+  print('Hello $name, nice to meet you.');
+  print('1 + 1 = ${1 + 1}');
+  print(r'Dia baru saja membeli komputer seharga $1,000.00');
+  print('Hi \u2665');
+}
